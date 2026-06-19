ec865aeb4e486e783cbdb5b17d48c7c0ea1c78d8
sandarukaweesha <sandarukaweesha404@gmail.com>
2026-06-19 18:10:03 +0530

    connected to database

---
+++ config/database.php
@@ -0,0 +1,37 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = "ellehub-mysql.mysql.database.azure.com";
+$dbname = "elle_hub";
+$username = "ellehubadmin";
+$password = "admin@1234";
+
+$conn = mysqli_init();
+
+mysqli_ssl_set(
+    $conn,
+    NULL,
+    NULL,
+    __DIR__ . "/../certs/DigiCertGlobalRootG2.crt.pem",
+    NULL,
+    NULL
+);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $username,
+    $password,
+    $dbname,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$conn) {
+    die("Connection Failed");
+}
+
+echo "Connection Successful";
