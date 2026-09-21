880f0edbb57ab25da3fc519922b9f23cbecff03b
Sonali Sanjana <sonalispeiris@gmail.com>
2026-09-21 19:23:39 +0530

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -10,11 +10,10 @@ class Database
 
             // Local MySQL 8 connection
             $host = "127.0.0.1";
-            $port = "3306";
+            $port = "3307";
             $database = "ellehubnew"; // Change if your local DB has a different name
             $username = "root";
-            $password = "1234";
-            $password = "12345";
+            $password="12345";
 
             $dsn = "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4";
 
